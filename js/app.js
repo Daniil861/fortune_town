7cfc763df40877c05483b64c9bee7f0a60b13e18
Igor <90471703+Daniil861@users.noreply.github.com>
2022-04-27 13:44:22 +0300

    commit 2 - version project

--- js/app.js
+++ js/app.js
@@ -33,7 +33,6 @@
     }
     if (sessionStorage.getItem("money")) {
         if (document.querySelector(".check")) document.querySelectorAll(".check").forEach((el => {
-            console.log(el);
             el.textContent = sessionStorage.getItem("money");
         }));
     } else {
@@ -57,28 +56,40 @@
             document.querySelectorAll(block).forEach((el => el.textContent = sessionStorage.getItem("money")));
         }), 500);
         setTimeout((() => {
-            document.querySelectorAll(block).forEach((el => el.classList.add("_delete-money")));
+            document.querySelectorAll(block).forEach((el => el.classList.remove("_delete-money")));
         }), 1500);
     }
     function no_money(block) {
         document.querySelectorAll(block).forEach((el => el.classList.add("_no-money")));
         setTimeout((() => {
-            document.querySelectorAll(block).forEach((el => el.classList.add("_no-money")));
+            document.querySelectorAll(block).forEach((el => el.classList.remove("_no-money")));
         }), 1e3);
     }
     function get_random(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
     }
+    function add_money(count, block, delay, delay_off) {
+        setTimeout((() => {
+            document.querySelectorAll(block).forEach((el => el.textContent = +sessionStorage.getItem("money") + count));
+            document.querySelectorAll(block).forEach((el => el.classList.add("_anim-add-money")));
+            sessionStorage.setItem("money", +sessionStorage.getItem("money") + count);
+        }), delay);
+        setTimeout((() => {
+            document.querySelectorAll(block).forEach((el => el.classList.remove("_anim-add-money")));
+        }), delay_off);
+    }
     function get_random_animate() {
-        let number = get_random(0, 3);
-        let arr = [ "jump", "scale", "rotate" ];
+        let number = get_random(0, 4);
+        let arr = [ "jump", "scale", "rotate", "rotate-2" ];
         let block_icon = document.querySelector(".bank__body img");
-        if (block_icon.classList.contains("_anim-icon-jump")) block_icon.classList.remove("_anim-icon-jump"); else if (block_icon.classList.contains("_anim-icon-scale")) block_icon.classList.remove("_anim-icon-scale"); else if (block_icon.classList.contains("_anim-icon-rotate")) block_icon.classList.remove("_anim-icon-rotate");
-        block_icon.classList.add(`_anim-icon-${arr[number]}`);
+        if (block_icon.classList.contains("_anim-icon-jump")) block_icon.classList.remove("_anim-icon-jump"); else if (block_icon.classList.contains("_anim-icon-scale")) block_icon.classList.remove("_anim-icon-scale"); else if (block_icon.classList.contains("_anim-icon-rotate")) block_icon.classList.remove("_anim-icon-rotate"); else if (block_icon.classList.contains("_anim-icon-rotate-2")) block_icon.classList.remove("_anim-icon-rotate-2");
+        setTimeout((() => {
+            block_icon.classList.add(`_anim-icon-${arr[number]}`);
+        }), 100);
     }
     if (document.querySelector(".bank__body img")) setInterval((() => {
         get_random_animate();
-    }), 2e4);
+    }), 1e4);
     let config_shop = {
         price_chip_1: 4e3,
         price_chip_2: 6e3,
@@ -136,6 +147,113 @@
         image.setAttribute("alt", `Icon`);
         document.querySelector(".block-bet__icon").append(image);
     }
+    let config_game = {
+        numbers_bet: [],
+        numbers_cubs: [],
+        same_numbers_cubs: false,
+        current_win: 0,
+        cub_1_top: 0,
+        cub_1_left: 0,
+        cub_2_top: 0,
+        cub_2_left: 0,
+        cub_3_top: 0,
+        cub_3_left: 0
+    };
+    function create_chip_field_bet(block) {
+        let item = document.createElement("div");
+        item.classList.add("field__chip");
+        let image = document.createElement("img");
+        let current_chip = sessionStorage.getItem("current-chip");
+        image.setAttribute("src", `img/icons/chip-${current_chip}.png`);
+        image.setAttribute("alt", "Icon");
+        item.append(image);
+        block.append(item);
+        block.classList.add("_not-active");
+    }
+    function get_number_item_field(block) {
+        return block.dataset.lot;
+    }
+    function add_delete_class(block) {
+        block.classList.add("_closed");
+        setTimeout((() => {
+            block.classList.remove("_closed");
+        }), 500);
+    }
+    function generate_coord_cubs() {
+        config_game.cub_1_top = `${get_random(0, 20)}%`;
+        config_game.cub_1_left = `${get_random(0, 55)}%`;
+        config_game.cub_2_top = `${get_random(50, 75)}%`;
+        config_game.cub_2_left = `${get_random(-10, 5)}%`;
+        config_game.cub_3_top = `${get_random(50, 75)}%`;
+        config_game.cub_3_left = `${get_random(50, 60)}%`;
+    }
+    function move_cubs_to_coordinate() {
+        document.querySelector(".cubs__cub_1").style.top = config_game.cub_1_top;
+        document.querySelector(".cubs__cub_1").style.left = config_game.cub_1_left;
+        document.querySelector(".cubs__cub_2").style.top = config_game.cub_2_top;
+        document.querySelector(".cubs__cub_2").style.left = config_game.cub_2_left;
+        document.querySelector(".cubs__cub_3").style.top = config_game.cub_3_top;
+        document.querySelector(".cubs__cub_3").style.left = config_game.cub_3_left;
+        document.querySelectorAll(".cubs__cub").forEach((el => {
+            el.style.transform = "rotate(0deg)";
+        }));
+    }
+    function move_cubs_to_start_coordinate() {
+        document.querySelector(".cubs__cub_1").style.top = "0%";
+        document.querySelector(".cubs__cub_1").style.left = "-10%";
+        document.querySelector(".cubs__cub_2").style.top = "0%";
+        document.querySelector(".cubs__cub_2").style.left = "-10%";
+        document.querySelector(".cubs__cub_3").style.top = "0%";
+        document.querySelector(".cubs__cub_3").style.left = "-10%";
+        document.querySelectorAll(".cubs__cub").forEach((el => {
+            el.style.transform = "rotate(-360deg)";
+        }));
+    }
+    function write_value_cubs() {
+        document.querySelectorAll(".cubs__cub img").forEach((el => el.remove()));
+        let image_1 = document.createElement("img");
+        let image_2 = document.createElement("img");
+        let image_3 = document.createElement("img");
+        config_game.numbers_cubs.push(get_random(1, 7));
+        config_game.numbers_cubs.push(get_random(1, 7));
+        config_game.numbers_cubs.push(get_random(1, 7));
+        image_1.setAttribute("src", `img/game/cub-${config_game.numbers_cubs[0]}.png`);
+        image_2.setAttribute("src", `img/game/cub-${config_game.numbers_cubs[1]}.png`);
+        image_3.setAttribute("src", `img/game/cub-${config_game.numbers_cubs[2]}.png`);
+        document.querySelector(".cubs__cub_1").append(image_1);
+        document.querySelector(".cubs__cub_2").append(image_2);
+        document.querySelector(".cubs__cub_3").append(image_3);
+    }
+    function check_win() {
+        check_same_numbers_cube();
+        if (config_game.same_numbers_cubs) {
+            if (config_game.numbers_cubs[0] == config_game.numbers_bet[0]) config_game.current_win = 10 * +sessionStorage.getItem("current-bet"); else if (config_game.numbers_cubs[0] == config_game.numbers_bet[1]) config_game.current_win = 10 * +sessionStorage.getItem("current-bet"); else if (config_game.numbers_cubs[0] == config_game.numbers_bet[2]) config_game.current_win = 10 * +sessionStorage.getItem("current-bet");
+        } else config_game.numbers_cubs.forEach((el => {
+            if (config_game.numbers_bet[0]) if (el == config_game.numbers_bet[0]) config_game.current_win += 2 * +sessionStorage.getItem("current-bet");
+            if (config_game.numbers_bet[1]) if (el == config_game.numbers_bet[1]) config_game.current_win += 2 * +sessionStorage.getItem("current-bet");
+            if (config_game.numbers_bet[2]) if (el == config_game.numbers_bet[2]) config_game.current_win += 2 * +sessionStorage.getItem("current-bet");
+        }));
+        if (config_game.current_win > 0) {
+            document.querySelector(".win__text").textContent = config_game.current_win;
+            add_remove_className(".win", "_active");
+            add_money(config_game.current_win, ".check", 1e3, 1500);
+        } else add_remove_className(".loose", "_active");
+    }
+    function check_same_numbers_cube() {
+        if (config_game.numbers_cubs[0] == config_game.numbers_cubs[1] && config_game.numbers_cubs[1] == config_game.numbers_cubs[2]) config_game.same_numbers_cubs = true;
+    }
+    function reset_game() {
+        move_cubs_to_start_coordinate();
+        document.querySelector(".block-bet").classList.remove("_hold");
+        config_game.numbers_bet = [];
+        config_game.numbers_cubs = [];
+        config_game.current_win = 0;
+        config_game.same_numbers_cubs = false;
+        if (document.querySelector(".field__chip")) document.querySelectorAll(".field__chip").forEach((el => el.remove()));
+        document.querySelectorAll(".field__item").forEach((el => {
+            if (el.classList.contains("_not-active")) el.classList.remove("_not-active");
+        }));
+    }
     document.addEventListener("click", (e => {
         let targetElement = e.target;
         if (targetElement.closest(".preloader__button")) {
@@ -151,13 +269,12 @@
         if (targetElement.closest(".main__link_play")) {
             create_chip();
             add_remove_className(".main", "_hide");
-            add_remove_className(".game", "_hide");
-            sessionStorage.setItem("current-bet", 100);
+            if (+sessionStorage.getItem("money") > 0) sessionStorage.setItem("current-bet", 100); else sessionStorage.setItem("current-bet", 0);
             document.querySelector(".block-bet__coins").textContent = sessionStorage.getItem("current-bet");
         }
         if (targetElement.closest(".button_home")) {
             add_remove_className(".main", "_hide");
-            if (!document.querySelector(".shop").classList.contains("_hide")) add_remove_className(".shop", "_hide"); else if (!document.querySelector(".game").classList.contains("_hide")) add_remove_className(".game", "_hide");
+            if (!document.querySelector(".shop").classList.contains("_hide")) add_remove_className(".shop", "_hide"); else reset_game();
         }
         if (targetElement.closest(".shop__button_4")) if (sessionStorage.getItem("chips-4")) {
             get_current_chip(4);
@@ -167,7 +284,6 @@
         if (targetElement.closest(".shop__button_2")) choice_chip(2, targetElement.closest(".shop__item"));
         if (targetElement.closest(".shop__button_1")) choice_chip(1, targetElement.closest(".shop__item"));
         if (targetElement.closest(".block-bet__minus")) {
-            console.log("Нажали на кнопку минус");
             let current_bet = +sessionStorage.getItem("current-bet");
             if (current_bet >= 100) {
                 sessionStorage.setItem("current-bet", current_bet - 100);
@@ -175,13 +291,43 @@
             }
         }
         if (targetElement.closest(".block-bet__plus")) {
-            console.log("Нажали на кнопку плюс");
             let current_bet = +sessionStorage.getItem("current-bet");
             let current_bank = +sessionStorage.getItem("money");
             if (current_bank - 99 > current_bet) {
                 sessionStorage.setItem("current-bet", current_bet + 100);
                 document.querySelector(".block-bet__coins").textContent = sessionStorage.getItem("current-bet");
             } else no_money(".check");
+        }
+        if (targetElement.closest(".field__item") && +sessionStorage.getItem("money") >= +sessionStorage.getItem("current-bet")) if (config_game.numbers_bet.length < 3) {
+            create_chip_field_bet(targetElement.closest(".field__item"));
+            config_game.numbers_bet.push(get_number_item_field(targetElement.closest(".field__item")));
+            delete_money(+sessionStorage.getItem("current-bet"), ".check");
+            document.querySelector(".block-bet").classList.add("_hold");
+        } else add_delete_class(targetElement.closest(".field__item")); else if (targetElement.closest(".field__item") && +sessionStorage.getItem("money") < +sessionStorage.getItem("current-bet")) no_money(".check");
+        if (targetElement.closest(".bet__button") && config_game.numbers_bet.length > 0) {
+            add_remove_className(".bet__button", "_hold");
+            generate_coord_cubs();
+            move_cubs_to_coordinate();
+            setTimeout((() => {
+                write_value_cubs();
+            }), 300);
+            setTimeout((() => {
+                check_win();
+            }), 1700);
+        } else if (targetElement.closest(".bet__button") && 0 == config_game.numbers_bet.length) {
+            document.querySelector(".field__body").classList.add("_anim-block");
+            setTimeout((() => {
+                document.querySelector(".field__body").classList.remove("_anim-block");
+            }), 1e3);
+        }
+        if (targetElement.closest(".loose__button_play") || targetElement.closest(".win__button_play")) {
+            if (targetElement.closest(".loose__button_play")) add_remove_className(".loose", "_active"); else add_remove_className(".win", "_active");
+            add_remove_className(".bet__button", "_hold");
+            reset_game();
+            if (+sessionStorage.getItem("money") < +sessionStorage.getItem("current-bet")) {
+                +sessionStorage.setItem("current-bet", 0);
+                document.querySelector(".block-bet__coins").textContent = sessionStorage.getItem("current-bet");
+            }
         }
     }));
     window["FLS"] = true;
